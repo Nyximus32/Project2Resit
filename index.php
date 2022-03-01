d6adbf672692a3c387afedbf3d375857b0b24b01
Hasibahs <91316758+Hasibahs@users.noreply.github.com>
2022-03-01 10:24:25 +0100

    java script, owl carousel

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NHL WEBSHOP</title>
+    <link href="font/fontawesome-free-6.0.0-web/css/svg-with-js.css">
+    <link rel="stylesheet" href="css/jquery.bxslider.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="wrapper">
+        <div class="navigation-bar">
+            <div class="container">
+                 <!-- insert any logo -->
+                <div class="logo">
+                   <img src="images/NHL webshop 1.PNG" alt="" width="87px">
+                </div>
+                <!-- This is a search box -->
+                <div class="search-area">
+                    <form action="" method="post">
+                        <input type="text" name="search_box" class="search_box">
+                        <button class="search_button">search</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
+
+<script src="js/jquery.js"></script>
+<script src="js/jquery.bxslider.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
