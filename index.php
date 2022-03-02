7f9ed549f9fd29923672175e3b281b5d85a2bbb8
Hasibahs <91316758+Hasibahs@users.noreply.github.com>
2022-03-02 11:21:06 +0100

    Navigation menu css

--- index.php
+++ index.php
@@ -23,21 +23,39 @@
                 <!-- This is a search box -->
                 <div class="search-area">
                     <form action="" method="post">
-                        <input type="text" name="search_box" class="search_box">
-                        <button class="search_button">search</button>
+                        <input type="text" name="search_box" class="search_box" placeholder="Search all the items..">
+                        <button class="search_button"><i class="fa-solid fa-magnifying-glass"></i></button>
                     </form>
+                </div>
+                <!-- User Menu -->
+                <div class="user-menu">
+                    <ul>
+                        <li class="active">
+                            <a href="#"><i class="fas fa-user"></i>My account</a>
+                            <!-- dropdown box -->
+                            <div class="dropdown-menu"> <!-- dropdown-menu-right bg-dark" -->
+                                <ul>
+                                    <li><a href="#">Sign in</a></li>
+                                    <li><a href="#">Order</a></li>
+                                    <li><a href="#">Cart</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li><a href="#"><i class="fa-solid fa-cart-shopping"></i>Cart</a></li>
+                    </ul>
                 </div>
             </div>
         </div>
     </div>
     
 
-
+<i class="fas font"></i>
 <script src="js/jquery.js"></script>
 <script src="js/jquery.bxslider.min.js"></script>
 <script src="js/owl.carousel.min.js"></script>
 <script src="js/bootstrap.bundle.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
+<script src="font/fontawesome-free-6.0.0-web/js/all.js"></script>
 <script src="js/main.js"></script>
 </body>
 </html>
